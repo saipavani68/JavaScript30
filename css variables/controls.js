bc496ca0bbb4485f20d8946b2b528d767448a949
sai pavani nagisetti <pavaninagisetti@sais-MBP.lan>
2021-01-12 18:09:54 -0800

    CSS variables usage

---
+++ css variables/controls.js
@@ -0,0 +1,8 @@
+function handleUpdate(){
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+const inputs = document.querySelectorAll('.controls div input');
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
